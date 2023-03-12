32cc448893a25cd8bbad1e25fdebe8b5e0f458b6
EgorNXD <e.nechaev@g.nsu.ru>
2023-03-12 19:06:33 +0700

    2

--- project1.1.py
+++ project1.1.py
@@ -1,11 +1,14 @@
 starting_distance = 16637000000
 speed = 38241
+print("Enter time in days:")
 time = int(input())*24
 miles = starting_distance + time*speed
-kilometers = 1.60934*miles
-astronomical = 92955781.16*miles
-print(miles, ' ', kilometers, ' ', astronomical)
+kilometers = round(1.60934*miles)
+au = 92955781.16*miles
+print("Voyager-1 traveled:")
+print(miles, ' miles, ', kilometers, ' kilometers,  ', au, '  astronomical units.  ')
 
 wave_speed = 299792458*2.23694
 wave_delay = miles/wave_speed
-print(wave_delay)
+print("Communication delay:")
+print(wave_delay, " hours.")
